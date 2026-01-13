9bd41cc2dfa60ed3c83c5a681439dd3f78e686ee
daniel <daniel@sephia.io>
2026-01-14 00:28:40 +0100

    polylines are unbounded, so mark dirty just in case

--- native/rolyrine/src/lib.rs
+++ native/rolyrine/src/lib.rs
@@ -2,14 +2,14 @@ use geo_types::LineString;
 use polyline::{decode_polyline, encode_coordinates};
 use rustler::NifResult;
 
-#[rustler::nif]
+#[rustler::nif(schedule = "DirtyCpu")]
 fn decode_nif(encoded: String, precision: u32) -> NifResult<Vec<(f64, f64)>> {
     decode_polyline(&encoded, precision)
         .map(|ls| ls.coords().map(|c| (c.x, c.y)).collect())
         .map_err(|e| rustler::Error::Term(Box::new(format!("{:?}", e))))
 }
 
-#[rustler::nif]
+#[rustler::nif(schedule = "DirtyCpu")]
 fn encode_nif(coords: Vec<(f64, f64)>, precision: u32) -> NifResult<String> {
     let linestring: LineString<f64> = coords.into_iter().collect();
     encode_coordinates(linestring, precision)
